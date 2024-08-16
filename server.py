c6854a29932a4026f9c0d2da9e77b1e8f04540e3
Koichiro Mori <f2forest@gmail.com>
2024-08-16 17:21:17 +0900

    LangServeのサンプルサーバ

---
+++ server.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
+
+system_template = "以下を {language} に翻訳してください"
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_template),
+        ("user", "{text}"),
+    ]
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+chain = prompt_template | model | parser
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="A simple API server using LangChain's Runnable interfaces",
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
