6c6d87d3ebfa6410b8fc0ec724adbc3d0031ef0e
Koichiro Mori <f2forest@gmail.com>
2024-08-30 13:50:53 +0900

    langserveのサンプルコードを追加

---
+++ langserve/20240830-retrieval.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+
+from langserve import add_routes
+
+vectorstore = FAISS.from_texts(
+    ["cats like fish", "dogs like sticks"], embedding=OpenAIEmbeddings()
+)
+retriever = vectorstore.as_retriever()
+
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="Spin up a simple api server using Langchain's Runnable interfaces",
+)
+
+add_routes(app, retriever)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
